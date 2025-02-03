eda1f75c4cc86a4873a7f536ef902c6d776115c7
Tawbe <tbennani091@gmail.com>
2025-02-03 21:12:40 +0100

    correction courseinfo part1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -76,14 +76,5 @@ const App = () => {
   );
 };
 
-function foo(x, condition) {
-  if (condition) {
-    console.log(x);
-    const x = 2;
-    console.log(x);
-  }
-}
-
-foo(1, true);
 
 export default App;
