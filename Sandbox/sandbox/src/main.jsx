e4af86a6cc6ff62c10ae45ed9386b0429378bfa4
Tawbe <tbennani091@gmail.com>
2025-02-03 17:27:40 +0100

    Test markdown part0

---
+++ Sandbox/sandbox/src/main.jsx
@@ -0,0 +1,24 @@
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App notes={notes} />
+);
